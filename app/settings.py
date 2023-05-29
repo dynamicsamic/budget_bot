e125adeea0a35b9a0e43205ae54ca504cba5ac8b
dynamicsamic <mirabyansr@gmail.com>
2023-05-29 23:17:35 +0300

    feat: add new project settings

--- app/settings.py
+++ app/settings.py
@@ -1,3 +1,11 @@
+from pathlib import Path
+
 from pytz import timezone
 
+ROOT_DIR = Path(__name__).resolve().parent
 TIME_ZONE = timezone("Europe/Moscow")
+DEBUG = True
+DATABASE = {
+    "prod_db_url": f"sqlite:///{ROOT_DIR}/db/prod.sqlite3",
+    "test_db_url": f"sqlite:///{ROOT_DIR}/db/test.sqlite3",
+}
