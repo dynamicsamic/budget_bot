5f6bd6762c78d85ddeedd1c8780b794fbea7a3c6
dynamicsamic <mirabyansr@gmail.com>
2023-06-03 22:28:00 +0300

    feat: add several properties for retrieving model attribute info

--- app/db/managers.py
+++ app/db/managers.py
@@ -4,6 +4,7 @@ from sqlalchemy import func, select, text
 from sqlalchemy.engine.result import ScalarResult
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import Session
+from sqlalchemy.orm.attributes import QueryableAttribute
 
 from app import settings
 from app.utils import session_agnostic
@@ -16,6 +17,31 @@ class QueryManager:
     def __init__(self, model: Type[BaseModel]) -> None:
         self.model = model
         self.db_engine = test_engine if settings.DEBUG else prod_engine
+
+    @property
+    def model_fields(self) -> dict[str, Type[QueryableAttribute]]:
+        return {
+            attr_name: attr_obj
+            for attr_name, attr_obj in self.model.__dict__.items()
+            if not attr_name.startswith("_")
+            and hasattr(attr_obj, "primary_key")
+        }
+
+    @property
+    def model_fieldnames(self) -> list[str]:
+        return list(self.model_fields.keys())
+
+    @property
+    def model_fieldtypes(self) -> list[str]:
+        return [field.type for field in self.model_fields.values()]
+
+    @property
+    def model_primary_keys(self) -> list[str]:
+        return [
+            fieldname
+            for fieldname, field_obj in self.model_fields.items()
+            if getattr(field_obj, "primary_key")
+        ]
 
     @session_agnostic
     def count(self, session: Session = None) -> int:
@@ -34,30 +60,24 @@ class QueryManager:
     @session_agnostic
     def get(
         self,
-        value: Any = None,
+        pk_value: Any = None,
         *,
         session: Session = None,
         **kwargs,
     ) -> Type[BaseModel] | None:
         """Return one instance of `self.model`."""
-        if value:
-            if primary_keys := [
-                attr_name
-                for attr_name, attr in self.model.__dict__.items()
-                if not attr_name.startswith("_")
-                and hasattr(attr, "primary_key")
-                and getattr(attr, "primary_key")
-            ]:
-                return session.get(self.model, {primary_keys[0]: value})
+        if pk_value:
+            if pk := self.model_primary_keys[0]:
+                return session.get(self.model, {pk: pk_value})
             # maybe need to raise error in else clause
 
         elif kwargs:
-            attr, value = list(kwargs.items())[0]
+            attr, pk_value = list(kwargs.items())[0]
             try:
                 return session.scalar(
                     select(self.model)
                     .filter(text(f"{attr}=:value"))
-                    .params(value=value)
+                    .params(value=pk_value)
                 )
             except SQLAlchemyError:
                 return
