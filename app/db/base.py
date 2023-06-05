71d7de6faf96fed9a7e2cb26e62e7c1685dfe061
dynamicsamic <mirabyansr@gmail.com>
2023-06-05 23:23:21 +0300

    feat: create `ModelFieldsDetails` mixin and add it to `AbstractBaseModel`

--- app/db/base.py
+++ app/db/base.py
@@ -1,17 +1,52 @@
 import datetime as dt
 from functools import cache
+from typing import Type
 
 from sqlalchemy import DateTime
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm.attributes import QueryableAttribute
 
 from app import settings
+
+
+class ModelFieldsDetails:
+    """Mixin that adds information about actual sqlalchemy table fields."""
+
+    @classmethod
+    @property
+    def fields(cls) -> dict[str, Type[QueryableAttribute]]:
+        return {
+            attr_name: attr_obj
+            for attr_name, attr_obj in cls.__dict__.items()
+            if not attr_name.startswith("_")
+            and hasattr(attr_obj, "primary_key")
+        }
+
+    @classmethod
+    @property
+    def fieldnames(cls) -> list[str]:
+        return list(cls.fields.keys())
+
+    @classmethod
+    @property
+    def fieldtypes(cls) -> list[str]:
+        return [field.type for field in cls.fields.values()]
+
+    @classmethod
+    @property
+    def primary_keys(cls) -> list[str]:
+        return [
+            fieldname
+            for fieldname, field_obj in cls.fields.items()
+            if getattr(field_obj, "primary_key")
+        ]
 
 
 class Base(DeclarativeBase):
     pass
 
 
-class BaseModel(Base):
+class BaseModel(Base, ModelFieldsDetails):
     __abstract__ = True
 
     id: Mapped[int] = mapped_column(primary_key=True)
@@ -23,9 +58,3 @@ class BaseModel(Base):
         default=dt.datetime.now(settings.TIME_ZONE),
         onupdate=dt.datetime.now(settings.TIME_ZONE),
     )
-
-    @classmethod
-    @property
-    @cache
-    def queries(cls):
-        return None
