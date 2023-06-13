c2698c60776d99d9f79874efaf06eed4b6a9c8ef
dynamicsamic <mirabyansr@gmail.com>
2023-06-13 22:14:13 +0300

    test: add new tests for `BaseModelManager` instance methods

--- tests/test_model_managers.py
+++ tests/test_model_managers.py
@@ -2,6 +2,8 @@ import pytest
 from sqlalchemy import select
 from sqlalchemy.engine.result import ScalarResult
 
+from app.db.managers import BaseModelManager
+
 from .conf import constants
 from .fixtures import (
     TestModel,
@@ -11,22 +13,6 @@ from .fixtures import (
     populate_db,
     test_manager,
 )
-
-
-def test_count_method_return_number_of_all_instances(test_manager):
-    num_entries = test_manager.count()
-    assert num_entries == constants["TEST_SAMPLE_SIZE"]
-    assert isinstance(num_entries, int)
-
-
-def test_all_method_without_args_return_scalar_result(test_manager):
-    model_instances = test_manager.all()
-    assert isinstance(model_instances, ScalarResult)
-
-
-def test_all_method_with_to_list_arg_return_list(test_manager):
-    model_instances = test_manager.all(to_list=True)
-    assert isinstance(model_instances, list)
 
 
 def test_get_method_with_valid_id_find_obj(test_manager):
@@ -77,8 +63,27 @@ def test_get_by_method_with_invalid_kwarg_return_none(test_manager):
     assert test_manager.get_by(**invalid_kwargs) is None
 
 
-def test_get_method_without_kwargs_return_none(test_manager):
+def test_get_by_method_without_kwargs_return_none(test_manager):
     assert test_manager.get_by() is None
+
+
+def test_all_method_without_args_return_scalar_result(test_manager):
+    model_instances = test_manager.all()
+    assert isinstance(model_instances, ScalarResult)
+
+
+def test_all_method_without_args_result_is_iterable(test_manager):
+    model_instances = test_manager.all()
+    assert all(isinstance(obj, TestModel) for obj in model_instances)
+
+
+def test_all_method_with_to_list_arg_return_list(test_manager):
+    model_instances = test_manager.all(to_list=True)
+    assert isinstance(model_instances, list)
+
+
+def test_all_method_return_empty_list_for_empty_table(db_session):
+    assert BaseModelManager(TestModel, db_session).all(to_list=True) == []
 
 
 def test_update_method_with_valid_id_without_kwargs_return_false(test_manager):
@@ -111,3 +116,45 @@ def test_update_method_with_commit_false_does_not_save_changes_to_db(
     name = "test_obj"
     assert test_manager.update(id_, commit=False, name=name) == True
     assert test_manager.get(id_).name != name
+
+
+def test_delete_method_with_valid_id_return_true(test_manager):
+    deleted = test_manager.delete(1)
+    assert isinstance(deleted, bool)
+    assert deleted == True
+
+
+def test_delete_method_with_unexisting_id_return_false(test_manager):
+    deleted = test_manager.delete(-1)
+    assert isinstance(deleted, bool)
+    assert deleted == False
+
+
+def test_delete_method_with_invalid_id_return_false(test_manager):
+    deleted = test_manager.delete("hello")
+    assert isinstance(deleted, bool)
+    assert deleted == False
+
+
+def test_count_method_return_number_of_all_instances(test_manager):
+    num_entries = test_manager.count()
+    assert num_entries == constants["TEST_SAMPLE_SIZE"]
+    assert isinstance(num_entries, int)
+
+
+def test_exists_method_with_valid_id_return_true(test_manager):
+    exists = test_manager.exists(1)
+    assert isinstance(exists, bool)
+    assert exists == True
+
+
+def test_exists_method_with_unexisting_id_return_false(test_manager):
+    exists = test_manager.exists(-1)
+    assert isinstance(exists, bool)
+    assert exists == False
+
+
+def test_exists_method_with_invalid_id_return_false(test_manager):
+    exists = test_manager.exists("hello")
+    assert isinstance(exists, bool)
+    assert exists == False
