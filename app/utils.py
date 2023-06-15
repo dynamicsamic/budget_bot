0d07e86f8c045f8238d6b476acd840a2e4a5afcd
dynamicsamic <mirabyansr@gmail.com>
2023-06-15 22:46:52 +0300

    feat: delete  decorator as it's no longer used; add new datetime functions

--- app/utils.py
+++ app/utils.py
@@ -1,40 +1,21 @@
-from functools import wraps
-from typing import Protocol
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session, scoped_session, sessionmaker
+import datetime as dt
 
 from app import settings
-from app.db import base, prod_engine, test_engine
 
 
-class Myp(Protocol):
-    def __call__(self, session: Session, *args, **kwargs):
-        ...
+def today() -> dt.datetime:
+    return dt.datetime.today().astimezone(settings.TIME_ZONE)
 
 
-def session_agnostic(f: Myp):
-    """Decorator that injects a sqlalchemy session into class method."""
+def yesterday() -> dt.datetime:
+    t = today()
+    return dt.datetime(year=t.year, month=t.month, day=t.day - 1).astimezone(
+        settings.TIME_ZONE
+    )
 
-    @wraps(f)
-    def inner(self: base.BaseModel, *args, **kwargs):
-        session_exists = False
-        if args:
-            for arg in args:
-                if isinstance(arg, (Session, scoped_session)):
-                    session_exists = True
-        if not session_exists and kwargs:
-            for kwarg_value in kwargs.values():
-                if isinstance(kwarg_value, (Session, scoped_session)):
-                    session_exists = True
-        if session_exists:
-            return f(self, *args, **kwargs)
-        if hasattr(self, "db_engine") and self.db_engine is not None:
-            session = scoped_session(sessionmaker(bind=self.db_engine))
-        else:
-            engine = test_engine if settings.DEBUG else prod_engine
-            session = scoped_session(sessionmaker(bind=engine))
-        kwargs["session"] = session
-        return f(self, *args, **kwargs)
 
-    return inner
+def tomorrow() -> dt.datetime:
+    t = today()
+    return dt.datetime(year=t.year, month=t.month, day=t.day + 1).astimezone(
+        settings.TIME_ZONE
+    )
