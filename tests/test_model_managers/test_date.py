d0b2516969ca20cca461f4d6e350c042ef2c2f3d
dynamicsamic <mirabyansr@gmail.com>
2023-06-20 21:13:52 +0300

    test: add tests for `DateQueryManager` methods

--- tests/test_model_managers/test_date.py
+++ tests/test_model_managers/test_date.py
@@ -1,6 +1,7 @@
 import datetime as dt
 
 import pytest
+from sqlalchemy.engine.result import ScalarResult
 
 from app import settings
 from app.utils import today, tomorrow, yesterday
@@ -33,19 +34,61 @@ def test_last_method_return_last_added_instance(db_session, date_manager):
     assert last_from_db.name == "new_obj"
 
 
-@pytest.mark.current
 def test_first_n_return_given_number_of_first_added_instances(
     db_session, date_manager
 ):
+    sample_size = 5
+    test_names = [f"new_obj{i}" for i in range(sample_size)]
+
     db_session.add_all(
         [
-            TestModel(name=f"new_obj{i}", created_at=tomorrow())
-            for i in range(5)
+            TestModel(name=test_name, created_at=yesterday())
+            for test_name in test_names
         ]
     )
     db_session.commit()
 
-    sample_size = 5
     first_n_from_db = date_manager.first_n(sample_size)
-    assert len(list(first_n_from_db)) == sample_size
-    print(type(date_manager.first_n(0)))
+    assert isinstance(first_n_from_db, ScalarResult)
+
+    first_n_from_db = first_n_from_db.all()
+    assert len(first_n_from_db) == sample_size
+
+    first_n_names = [obj.name for obj in first_n_from_db]
+    assert first_n_names == test_names
+
+
+def test_first_n_return_empty_result_for_zero_arg(date_manager):
+    first_n_from_db = date_manager.first_n(0)
+    assert isinstance(first_n_from_db, ScalarResult)
+    assert first_n_from_db.all() == []
+
+
+def test_last_n_return_given_number_of_first_added_instances(
+    db_session, date_manager
+):
+    sample_size = 5
+    test_names = [f"new_obj{i}" for i in range(sample_size)]
+
+    db_session.add_all(
+        [
+            TestModel(name=test_name, created_at=tomorrow())
+            for test_name in test_names
+        ]
+    )
+    db_session.commit()
+
+    last_n_from_db = date_manager.last_n(sample_size)
+    assert isinstance(last_n_from_db, ScalarResult)
+
+    last_n_from_db = last_n_from_db.all()
+    assert len(last_n_from_db) == sample_size
+
+    last_n_names = [obj.name for obj in last_n_from_db]
+    assert last_n_names == test_names
+
+
+def test_last_n_return_empty_result_for_zero_arg(date_manager):
+    last_n_from_db = date_manager.last_n(0)
+    assert isinstance(last_n_from_db, ScalarResult)
+    assert last_n_from_db.all() == []
