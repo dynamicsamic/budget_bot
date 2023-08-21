b0cc8aecc6b716bb57eb51fb96b62f679f5f3560
dynamicsamic <mirabyansr@gmail.com>
2023-08-21 23:17:03 +0300

    refactor: major refacor for manager classes; fewer abstraction, add attribute validation and more

--- app/db/managers.py
+++ app/db/managers.py
@@ -1,10 +1,11 @@
 import datetime as dt
 import operator
+import re
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import Any, Callable, Iterable, Literal, Type
+from typing import Any, Callable, Iterable, Literal, Sequence, Type
 
-from sqlalchemy import and_
+from sqlalchemy import Date, DateTime, and_
 from sqlalchemy import func as sql_func
 from sqlalchemy import select, text
 from sqlalchemy.orm import Query, Session, scoped_session
@@ -14,61 +15,25 @@ from app.db.base import AbstractBaseModel
 from app.utils import DateGen
 
 from .base import AbstractBaseModel
+from .exceptions import InvalidDatefield, InvalidFilter, InvalidOrderByValue
 from .models import Entry, User
 
 
-@dataclass
-class AbstractModelManager(ABC):
-    """Interface for creating model managers."""
-
-    model: Type[AbstractBaseModel]
-    session: Session | scoped_session
-
-    @abstractmethod
-    def get(self, id: int) -> Type[AbstractBaseModel] | None:
-        """Select one `self.model` object by it's id."""
-        pass
-
-    @abstractmethod
-    def get_by(self, **kwargs) -> Type[AbstractBaseModel] | None:
-        """Select one `self.model` object by kwargs."""
-        pass
-
-    @abstractmethod
-    def all(self, **kwargs) -> Iterable[Type[AbstractBaseModel]]:
-        """Select all `self.model` objects."""
-        pass
-
-    @abstractmethod
-    def update(self, id: int, **kwargs) -> bool:
-        """Update one `self.model` object with given id with given kwargs."""
-        pass
-
-    @abstractmethod
-    def delete(self, id: int) -> bool:
-        """Delete one `self.model` object with given id."""
-        pass
-
-    @abstractmethod
-    def count(self) -> int:
-        """Count number of all `self.model` objects."""
-        pass
-
-    @abstractmethod
-    def exists(self, id: int) -> bool:
-        """Tell wether `self.model` object with given id exists or not."""
-        pass
-
-
-class BaseModelManager(AbstractModelManager):
+class ModelManager:
     def __init__(
         self,
         model: Type[AbstractBaseModel],
         session: Session | scoped_session = None,
-        /,
+        order_by: Sequence[str] = ["created_at", "last_updated", "id"],
+        filters: Sequence[str] = None,  #  like ["id>2", "sum == 1"]
+        datefield: str = "created_at",
     ) -> None:
         self.model = model
         self.session = session
+        self._order_by = order_by
+        self._filters = filters
+        self._datefield = datefield
+        self._validate()
 
     def __call__(self, session: Session | scoped_session) -> None:
         """Allow `session` be associated
@@ -88,26 +53,35 @@ class BaseModelManager(AbstractModelManager):
         ):
             self.session = session
 
-    def get(self, id: int) -> Type[AbstractBaseModel] | None:
-        """Retrieve `self.model` object."""
-        return self.session.get(self.model, id)
+    @property
+    def order_by(self) -> Sequence[str]:
+        return self._order_by
 
-    def get_by(self, **kwargs) -> Type[AbstractBaseModel] | None:
-        """Retrieve `self.model` object filtered by kwargs."""
-        if valid_kwargs := self.clean_kwargs(kwargs):
-            return (
-                self.session.query(self.model)
-                .filter_by(**valid_kwargs)
-                .first()
-            )
+    @order_by.setter
+    def order_by(self, order_by_: Sequence[str]) -> None:
+        self._validate_order_by(order_by_)
+        self._order_by = order_by_
 
-    def all(self) -> Query[Type[AbstractBaseModel]]:
-        """Retrieve all `self.model` objets."""
-        return self.session.query(self.model)
+    @property
+    def filters(self) -> Sequence[str]:
+        return self._filters
 
-    def list(self) -> list[Type[AbstractBaseModel]]:
-        """Retrieve all `self.model` objets in list."""
-        return self.all().all()
+    @filters.setter
+    def filters(self, filters_: Sequence[str]) -> None:
+        self._validate_filters(filters_)
+        self._filters = filters_
+
+    @property
+    def datefield(self) -> str:
+        return self._datefield
+
+    @datefield.setter
+    def datefield(self, datefield_: str) -> None:
+        self._validate_datefield(datefield_)
+        self._datefield = datefield_
+
+    def create(self, **kwargs):
+        pass
 
     def update(
         self,
@@ -117,7 +91,7 @@ class BaseModelManager(AbstractModelManager):
         **kwargs,
     ) -> bool:
         """Update `self.model` object."""
-        if valid_kwargs := self.clean_kwargs(kwargs):
+        if valid_kwargs := self._clean_kwargs(kwargs):
             updated = bool(
                 self.session.query(self.model)
                 .filter_by(id=id)
@@ -142,6 +116,34 @@ class BaseModelManager(AbstractModelManager):
         except Exception:
             return False
 
+    def get(self, id: int) -> Type[AbstractBaseModel] | None:
+        """Retrieve `self.model` object."""
+        return self.session.get(self.model, id)
+
+    def get_by(self, **kwargs) -> Type[AbstractBaseModel] | None:
+        """Retrieve `self.model` object filtered by kwargs."""
+        if valid_kwargs := self._clean_kwargs(kwargs):
+            return (
+                self.session.query(self.model)
+                .filter_by(**valid_kwargs)
+                .first()
+            )
+
+    def all(self) -> Query[Type[AbstractBaseModel]]:
+        """Retrieve all `self.model` objets."""
+        return self._fetch()
+
+    def list(self) -> list[Type[AbstractBaseModel]]:
+        """Retrieve all `self.model` objets in list."""
+        return self._fetch().all()
+
+    def select(
+        self,
+        reverse: bool = False,
+        *filters: str,
+    ) -> Query[Type[AbstractBaseModel]]:
+        return self._fetch(reverse, *filters)
+
     def count(self) -> int:
         """Calculate number of all `self.model` objects."""
         return self.session.query(self.model.id).count()
@@ -152,88 +154,154 @@ class BaseModelManager(AbstractModelManager):
             self.session.scalar(select(self.model.id).filter_by(id=id))
         )
 
-    def clean_kwargs(self, kwargs: dict[str, Any]) -> dict[str, Any]:
+    def first(self, *filters: str) -> Type[AbstractBaseModel] | None:
+        """Retrieve first `model` instance in ascending query."""
+        return self.first_n(1, *filters).one_or_none()
+
+    def last(self, *filters: str) -> Type[AbstractBaseModel] | None:
+        """Retrieve last `model` instance in discending query."""
+        return self.last_n(1, *filters).one_or_none()
+
+    def first_n(self, n: int, *filters: str) -> Query[Type[AbstractBaseModel]]:
+        """Retrieve specific number of `model` instances
+        sorted in ascending order.
+        """
+        return self._fetch_n(n, *filters)
+
+    def last_n(self, n: int, *filters: str) -> Query[Type[AbstractBaseModel]]:
+        """Retrieve specific number of `model` instances
+        sorted in descending order.
+        """
+        return self._fetch_n(n, True, *filters)
+
+    def _clean_kwargs(self, kwargs: dict[str, Any]) -> dict[str, Any]:
         return {
             fieldname: value
             for fieldname, value in kwargs.items()
             if fieldname in self.model.fieldnames
         }
 
-
-class OrderedQueryManager(BaseModelManager):
-    def __init__(
-        self, *args, order_by: list[str] = ["created_at", "last_updated", "id"]
-    ):
-        self._order_by = order_by
-        return super().__init__(*args)
-
-    def all(self, reverse: bool = False) -> Query[Type[AbstractBaseModel]]:
-        """Retrieve all `model` instances in ordered query.
-        Use `reverse=True` to sort query in descending order.
-        """
-        qs = super().all()
-        return (
-            qs.order_by(text(self.reverse_order_by))
-            if reverse
-            else qs.order_by(text(self.order_by))
+    def _fetch(
+        self, reverse: bool = False, *filters: str
+    ) -> Query[Type[AbstractBaseModel]]:
+        q = self.session.query(self.model).order_by(
+            text(self._compile_order_by(reverse))
         )
+        filter_by = self._compile_filter_expr(filters)
+        if filter_by is not None:
+            return q.filter(filter_by)
 
-    def list(self, reverse: bool = False) -> list[Type[AbstractBaseModel]]:
-        """Retrieve all `self.model` objets in list."""
-        return self.all(reverse=reverse).all()
-
-    def first_n(self, n: int) -> Query[Type[AbstractBaseModel]]:
-        """Retrieve specific number of `model` instances
-        sorted in ascending order.
-        """
-        return self._fetch_n(n, self.order_by)
-
-    def first(self) -> Type[AbstractBaseModel] | None:
-        """Retrieve first `model` instance in ascending query."""
-        return self.first_n(1).one_or_none()
-
-    def last(self) -> Type[AbstractBaseModel] | None:
-        """Retrieve last `model` instance in discending query."""
-        return self.last_n(1).one_or_none()
+        return q
 
     def _fetch_n(
-        self, n: int, order_by: str = ""
+        self, n: int, reverse: bool = False, *filters: str
     ) -> Query[Type[AbstractBaseModel]]:
-        return self._fetch(order_by).limit(n)
+        return self._fetch(reverse, *filters).limit(n)
 
-    def _fetch(self, order_by: str = "") -> Query[Type[AbstractBaseModel]]:
-        return self.session.query(self.model).order_by(text(order_by))
+    def _compile_order_by(self, reverse=bool) -> str:
+        return (
+            self._reverse_order_by() if reverse else self._prepare_order_by()
+        )
 
-    @property
-    def order_by(self) -> str:
-        return ", ".join(self._order_by)
+    def _prepare_order_by(self) -> str:
+        return ", ".join(self.order_by)
 
-    @property
-    def reverse_order_by(self):
+    def _reverse_order_by(self) -> str:
         return ", ".join(
             (
                 item.removesuffix(" desc")
                 if item.endswith(" desc")
                 else item + " desc"
-                for item in self._order_by
+                for item in self.order_by
             )
         )
 
+    def _compile_filter_expr(self, filters: Sequence[str] = None):
+        filters = filters or self.filters
 
-class DateQueryManager(OrderedQueryManager):
-    def __init__(self, *args, datefield: str = "created_at", **kwargs):
-        self._datefield = datefield
-        return super().__init__(*args, **kwargs)
+        if not filters:
+            return
 
+        self._validate_filters(filters)
+
+        return and_(True, *[text(filter) for filter in filters])
+
+    def _validate(self):
+        self._validate_order_by()
+        self._validate_filters()
+        self._validate_datefield()
+
+    def _validate_order_by(self, order_by: Sequence[str] = None):
+        order_by = order_by or self.order_by
+        cleaned_values = {val.strip() for val in order_by}
+        if invalid_fields := cleaned_values - self.model.fieldnames:
+            raise InvalidOrderByValue(
+                f"""Following values can not be 
+                used as `order_by` args: {', '.join(invalid_fields)}."""
+            )
+
+    def _validate_filters(self, filters: Sequence[str] = None):
+        filters = filters or self.filters
+        if filters:
+            valid_signs = {
+                ">": "gt",
+                ">=": "ge",
+                "<": "lt",
+                "<=": "le",
+                "==": "eq",
+                "!=": "ne",
+            }
+            for filter in filters:
+                try:
+                    attr, sign, value = re.split("([<>!=]+)", filter)
+                except ValueError:
+                    raise InvalidFilter(
+                        """Filter should follow pattern:
+                          <model_attribute><compare_operator><value>.
+                          Example: `sum>1`, `id == 2`"""
+                    )
+
+                if not hasattr(self.model, attr.strip()):
+                    raise InvalidFilter(
+                        f"""Model `{self.model}` 
+                        does not have `{attr}` atribute."""
+                    )
+
+                elif not hasattr(operator, valid_signs.get(sign, "None")):
+                    raise InvalidFilter(f"Invalid comparing sign: `{sign}`")
+
+                elif not value:
+                    raise InvalidFilter("Filter must have a value.")
+
+    def _validate_datefield(self, datefield_: str = None):
+        datefield_ = datefield_ or self._datefield
+        if datefield := getattr(self.model, datefield_, None):
+            if not isinstance(datefield.type, (Date, DateTime)):
+                raise InvalidDatefield(
+                    "Datefield must be of sqlalchemy `Date` or `Datetime` types."
+                )
+        else:
+            raise InvalidDatefield(
+                f"""Model `{self.model}`
+                    does not have `{datefield_}` atribute."""
+            )
+
+
+class DateQueryManager(ModelManager):
     def today(
         self, date_info: DateGen, reverse: bool = False
     ) -> Query[Type[AbstractBaseModel]]:
         return self._between(*date_info.date_range, reverse)
 
     def yesterday(
-        self, date_info: DateGen, reverse: bool = False
+        self,
+        date_info: DateGen,
+        filter_expr: str = None,
+        reverse: bool = False,
     ) -> Query[Type[AbstractBaseModel]]:
-        return self._between(*date_info.yesterday_range, reverse)
+        return self._new_between(
+            *date_info.yesterday_range, filter_expr, reverse
+        )
 
     def this_week(
         self, date_info: DateGen, reverse: bool = False
@@ -259,8 +327,24 @@ class DateQueryManager(OrderedQueryManager):
         """Fetch all instances of `model` filtered
         between given borders.
         """
-        order_by = self.reverse_order_by if reverse else self.order_by
+        order_by = (
+            self._reverse_order_by if reverse else self._prepare_order_by
+        )
         return self._fetch(order_by).filter(
+            column(self._datefield).between(start, end)
+        )
+
+    def _new_between(
+        self,
+        start: dt.datetime | dt.date,
+        end: dt.datetime | dt.date,
+        reverse: bool = False,
+        *filters: str,
+    ) -> Query[Type[AbstractBaseModel]]:
+        """Fetch all instances of `model` filtered
+        between given borders.
+        """
+        return self._fetch(reverse, *filters).filter(
             column(self._datefield).between(start, end)
         )
 
