a1606e51cd9b34d2dbfa9d4e09391d9ca8f0959d
dynamicsamic <mirabyansr@gmail.com>
2023-11-28 23:50:18 +0300

    refactor: update middlewares

--- app/bot/middlewares.py
+++ app/bot/middlewares.py
@@ -2,12 +2,12 @@ import datetime as dt
 from typing import Any, Awaitable, Callable, Dict
 
 from aiogram import BaseMiddleware
-from aiogram.dispatcher.flags import get_flag
 from aiogram.types import CallbackQuery, Message, TelegramObject
 
 from app import settings
-from app.db import get_session, managers
-from app.utils import DateGen
+from app.db import get_session
+from app.services.user import get_user
+from app.utils import DateRange
 
 # TODO: restrict to only private chats in middleware
 
@@ -25,7 +25,7 @@ class DateInfoMiddleware(BaseMiddleware):
             datetime = event.message.date
         else:
             datetime = dt.datetime.now()
-        data["date_info"] = DateGen(datetime.astimezone(settings.TIME_ZONE))
+        data["date_info"] = DateRange(datetime.astimezone(settings.TIME_ZONE))
         return await handler(event, data)
 
 
@@ -37,29 +37,6 @@ class CurrentUserMiddleWare(BaseMiddleware):
         data: Dict[str, Any],
     ) -> Any:
         with get_session() as db_session:
-            data["user"] = managers.UserManager(session=db_session).get_by(
-                tg_id=event.from_user.id
-            )
+            data["user"] = get_user(db_session, tg_id=event.from_user.id)
+            data["db_session"] = db_session
         return await handler(event, data)
-
-
-class ModelManagerMiddleware(BaseMiddleware):
-    async def __call__(
-        self,
-        handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
-        event: CallbackQuery | Message,
-        data: Dict[str, Any],
-    ) -> Any:
-        model_managers: list[managers.BaseModelManager] = get_flag(
-            data, "model_managers"
-        )
-
-        if not model_managers:
-            return await handler(event, data)
-
-        with get_session() as db_session:
-            for manager in model_managers:
-                manager_name = f"{manager.model.__tablename__}_manager"
-                data[manager_name] = manager(db_session)
-
-            return await handler(event, data)
