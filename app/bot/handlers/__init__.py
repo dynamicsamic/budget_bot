ecb196acd43301bc4a936fcf35ea0eb4e23aff3f
dynamicsamic <mirabyansr@gmail.com>
2023-12-10 22:27:11 +0300

    feat: add new middlewares and user router

--- app/bot/handlers/__init__.py
+++ app/bot/handlers/__init__.py
@@ -1,12 +1,17 @@
 from aiogram import Router
 
-from app.bot.middlewares import CurrentUserMiddleWare
+from app.bot.middlewares import (
+    DbSessionMiddleWare,
+    IdentifyUserMiddleWare,
+    RedirectAnonymousUserMiddleWare,
+)
 
 from .budget import router as budget_router
 from .callbacks import router as callback_router
 from .category import router as category_router
 from .commands import router as command_router
 from .entry import router as entry_router
+from .user import router as user_router
 
 router = Router()
 router.include_routers(
@@ -15,6 +20,11 @@ router.include_routers(
     category_router,
     callback_router,
     entry_router,
+    user_router,
 )
-router.message.middleware(CurrentUserMiddleWare())
-router.callback_query.middleware(CurrentUserMiddleWare())
+router.message.middleware(DbSessionMiddleWare())
+router.message.middleware(IdentifyUserMiddleWare())
+router.message.middleware(RedirectAnonymousUserMiddleWare())
+router.callback_query.middleware(DbSessionMiddleWare())
+router.callback_query.middleware(IdentifyUserMiddleWare())
+router.callback_query.middleware(RedirectAnonymousUserMiddleWare())
