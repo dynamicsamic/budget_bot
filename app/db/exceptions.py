bdbe2adfd774b73d84a908c523f2e10ba1f88532
dynamicsamic <mirabyansr@gmail.com>
2023-12-21 22:54:04 +0300

    feat: add new informative exceptions

--- app/db/exceptions.py
+++ app/db/exceptions.py
@@ -1,3 +1,8 @@
+from typing import Any
+
+from .custom_types import _BaseModel
+
+
 class BudgetBotDataBaseException(Exception):
     pass
 
@@ -32,3 +37,43 @@ class ModelInstanceCreateError(BudgetBotDataBaseException):
 
 class ImproperlyConfigured(BudgetBotDataBaseException):
     pass
+
+
+class ModelInstanceNotFound(BudgetBotDataBaseException):
+    pass
+
+
+class InvalidModelArgValue(BudgetBotDataBaseException):
+    def __init__(self, *args, model: _BaseModel, invalid_arg: Any) -> None:
+        super().__init__(*args)
+        self.model = model
+        self.invalid_arg = invalid_arg
+
+    def __repr__(self) -> str:
+        return (
+            "Недопустимый аргумент для модели: "
+            f"{self.model.__tablename__.capitalize()}: `{self.invalid_arg}`."
+        )
+
+
+class InvalidModelArgType(BudgetBotDataBaseException):
+    def __init__(
+        self,
+        *args,
+        model: _BaseModel,
+        arg_name: str,
+        expected_type: Any,
+        invalid_type: Any,
+    ) -> None:
+        super().__init__(*args)
+        self.model = model
+        self.arg_name = arg_name
+        self.expected_type = expected_type
+        self.invalid_type = invalid_type
+
+    def __repr__(self) -> str:
+        return (
+            f"Недопустимый тип для аргумента `{self.arg_name}` модели "
+            f"{self.model.__tablename__.capitalize()}: "
+            f"получен {self.invalid_type} вместо {self.expected_type}."
+        )
