a8ffa1a413ad453a1203276717c2c7be2bfd03fe
dynamicsamic <mirabyansr@gmail.com>
2023-12-21 22:56:10 +0300

    feat: add new return types for db queries

--- app/db/custom_types.py
+++ app/db/custom_types.py
@@ -1,4 +1,5 @@
-from typing import TypeVar
+from dataclasses import dataclass
+from typing import Any, Generator, Tuple, TypeVar
 
 from sqlalchemy.orm.attributes import InstrumentedAttribute
 from sqlalchemy.sql.elements import UnaryExpression
@@ -13,3 +14,41 @@ _ModelWithDatefield = TypeVar("_ModelWithDatefield", bound=AbstractBaseModel)
 _ModelWithCashflowfield = TypeVar(
     "_ModelWithCashflowfield", bound=AbstractBaseModel
 )
+
+
+@dataclass
+class GenericResult:
+    result: Any
+
+    def astuple(self) -> Tuple[Any]:
+        return tuple(self.__dict__.values())
+
+    def asdict(self) -> dict[str, Any]:
+        return self.__dict__
+
+
+@dataclass
+class ErrorAttachedResult(GenericResult):
+    error: Exception | None
+
+
+@dataclass
+class ModelCreateResult(ErrorAttachedResult):
+    result: _BaseModel | None
+
+
+@dataclass
+class ModelUpdateDeleteResult(ErrorAttachedResult):
+    result: bool | None
+
+
+@dataclass
+class ModelValidationResult(ErrorAttachedResult):
+    result: bool
+
+
+@dataclass
+class GeneratorResult(GenericResult):
+    result: Generator[_BaseModel, _BaseModel, None] | None
+    is_empty: bool
+    head: _BaseModel | None
