bcba1992e4073f6b1d4dcafc0cee01ae4b494e7b
dynamicsamic <mirabyansr@gmail.com>
2024-01-17 23:33:14 +0300

    feat: add new error handlers

--- app/bot/handlers/errors.py
+++ app/bot/handlers/errors.py
@@ -3,9 +3,20 @@ import logging
 from aiogram import Bot, Router, types
 from aiogram.filters import ExceptionTypeFilter
 from aiogram.fsm.context import FSMContext
+from sqlalchemy.exc import SQLAlchemyError
 
-from app.bot.prompts import callback_error_response
-from app.exceptions import InvalidCallbackData
+from app.bot import keyboards, prompts
+from app.exceptions import (
+    EmptyModelKwargs,
+    InvalidCallbackData,
+    InvalidCategoryName,
+    InvalidModelArgType,
+    InvalidModelAttribute,
+    ModelInstanceDuplicateAttempt,
+    ModelInstanceNotFound,
+    RepositoryValidationError,
+    UnknownDataBaseException,
+)
 from app.utils import aiogram_log_handler
 from config import config
 
@@ -13,10 +24,20 @@ logger = logging.getLogger(__name__)
 logger.addHandler(aiogram_log_handler)
 
 router = Router(name=__name__)
+serverside_errors = (
+    RepositoryValidationError,
+    InvalidCallbackData,
+    UnknownDataBaseException,
+    EmptyModelKwargs,
+    InvalidModelArgType,
+    InvalidModelAttribute,
+    SQLAlchemyError,
+    ModelInstanceNotFound,
+)
 
 
-@router.errors(ExceptionTypeFilter(InvalidCallbackData))
-async def invalid_callback_data_handler(
+@router.errors(ExceptionTypeFilter(*serverside_errors))
+async def serverside_error_handler(
     error_event: types.ErrorEvent, bot: Bot, state: FSMContext
 ):
     error_message = (
@@ -24,9 +45,38 @@ async def invalid_callback_data_handler(
         f"while processing {error_event.update}"
     )
     logger.error(error_message)
+
     support_id = int(config.support_manager_id.get_secret_value())
     await bot.send_message(support_id, text=error_message)
-    await error_event.update.callback_query.message.answer(
-        callback_error_response
-    )
+
+    update = error_event.update
+
+    if update.message is not None:
+        message = update.message
+    elif update.callback_query is not None:
+        message = update.callback_query.message
+
+    await message.answer(prompts.serverside_error_response)
     await state.clear()
+
+
+@router.errors(ExceptionTypeFilter(InvalidCategoryName))
+async def invalid_category_name_handler(error_event: types.ErrorEvent):
+    logger.info(f"Invalid user input triggered {error_event.exception}")
+    await error_event.update.message.answer(
+        "Недопустимое название категории."
+        f"{prompts.category_name_description}"
+    )
+
+
+@router.errors(ExceptionTypeFilter(ModelInstanceDuplicateAttempt))
+async def instance_duplicate_attempt_handler(error_event: types.ErrorEvent):
+    exception: ModelInstanceDuplicateAttempt = error_event.exception
+    await error_event.update.message.answer(
+        f"{exception}. Придумайте новое значение и повторите попытку "
+        "или прервите процедуру, нажав на кнопку отмены.",
+        reply_markup=keyboards.button_menu(keyboards.buttons.cancel_operation),
+    )
+    logger.info(
+        f"User {exception.user_tg_id} triggered exception: {repr(exception)}"
+    )
