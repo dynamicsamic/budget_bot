863f99ac6d4f310bac336fe05902978de594b5d0
dynamicsamic <mirabyansr@gmail.com>
2024-01-21 17:01:05 +0300

    refactor: first sketch for entry handlers with new repositories

--- app/bot/handlers/entry.py
+++ app/bot/handlers/entry.py
@@ -3,72 +3,68 @@ import datetime as dt
 from aiogram import F, Router, types
 from aiogram.filters.command import Command
 from aiogram.fsm.context import FSMContext
+from sqlalchemy.orm import Session, scoped_session
 
 from app import settings
-from app.bot import filters, keyboards
-from app.bot.middlewares import ModelManagerMiddleware
-from app.bot.states import EntryCreateState, EntryList
-from app.db.managers import DateQueryManager, EntryManager, ModelManagerStore
+from app.bot import filters, keyboards, states
+from app.bot.middlewares import EntryRepositoryMiddleWare
 from app.db.models import User
+from app.db.repository import CategoryRepository, EntryRepository
 
 router = Router()
-router.message.middleware(ModelManagerMiddleware())
-router.callback_query.middleware(ModelManagerMiddleware())
+
+router.message.middleware(EntryRepositoryMiddleWare())
+router.callback_query.middleware(EntryRepositoryMiddleWare())
 
 
 @router.message(Command("entry_create"))
 async def cmd_create_entry(
-    message: types.Message, state: FSMContext, user: User
+    message: types.Message,
+    state: FSMContext,
+    user: User,
+    db_session: Session | scoped_session,
 ):
     await message.answer(
         "Создаем новую транзакцию.\nВыберите бюджет",
-        reply_markup=keyboards.create_entry_show_budgets(user.budgets),
+        reply_markup=keyboards.create_entry_show_budgets(
+            # get_user_budgets(db_session, user.id)
+        ),
     )
-    await state.set_state(EntryCreateState.budget)
+    await state.set_state(states.CreateEntry.budget)
 
 
 @router.callback_query(
-    EntryCreateState.budget,
+    states.CreateEntry.budget,
     filters.EntryBudgetIdFilter(),
-    flags=ModelManagerStore.as_flags("budget"),
 )
 async def create_entry_receive_category(
     callback: types.CallbackQuery,
     state: FSMContext,
     user: User,
-    budget_id: int,
-    budget_manager: DateQueryManager,
+    ca_repository: CategoryRepository,
 ):
-    budget = budget_manager.get(budget_id)
-    if not budget:
-        await callback.message.answer(
-            "Указан неверный бюджет.\n" "Выберите бюджет из списка ниже.",
-            reply_markup=keyboards.create_entry_show_budgets(user.budgets),
-        )
-        return
-
-    await state.update_data(budget=budget)
     await callback.message.answer(
         "Выберите категорию",
-        reply_markup=keyboards.create_entry_show_categories(user.categories),
+        reply_markup=keyboards.create_entry_show_categories(
+            ca_repository.get_user_categories(user.id)
+        ),
     )
-    await state.set_state(EntryCreateState.category)
+    await state.set_state(states.CreateEntry.category)
     await callback.answer()
 
 
 @router.callback_query(
-    EntryCreateState.category,
+    states.CreateEntry.category,
     filters.EntryCategoryIdFilter(),
-    flags=ModelManagerStore.as_flags("category"),
 )
-async def create_entry_receive_category(
+async def create_entry_receive_category2(
     callback: types.CallbackQuery,
     state: FSMContext,
     user: User,
     category_id: int,
-    category_manager: DateQueryManager,
+    ca_repository: CategoryRepository,
 ):
-    category = category_manager.get(category_id)
+    category = ca_repository.get_category(category_id)
     if not category:
         await callback.message.answer(
             "Выбрана несуществуюшая.\n" "Выберите категорию из списка ниже.",
@@ -83,11 +79,11 @@ async def create_entry_receive_category(
         "Введите сумму транзакции.\n"
         "Допустимые виды записи: 1521; 100,91; 934.2; 1 445.67"
     )
-    await state.set_state(EntryCreateState.sum)
+    await state.set_state(states.CreateEntry.sum)
     await callback.answer()
 
 
-@router.message(EntryCreateState.sum, filters.EntrySumFilter())
+@router.message(states.CreateEntry.sum, filters.EntrySumFilter())
 async def create_entry_receive_sum(
     message: types.Message,
     state: FSMContext,
@@ -110,10 +106,10 @@ async def create_entry_receive_sum(
         "Отправьте точку `.`, если транзакция выполнена сегодня.\n"
         "Допустимые форматы: дд мм гггг, дд-мм-гггг, ддммгггг."
     )
-    await state.set_state(EntryCreateState.transcation_date)
+    await state.set_state(states.CreateEntry.transcation_date)
 
 
-@router.message(EntryCreateState.transcation_date, filters.EntryDateFilter())
+@router.message(states.CreateEntry.transcation_date, filters.EntryDateFilter())
 async def create_entry_receive_transaction_date(
     message: types.Message,
     state: FSMContext,
@@ -128,50 +124,45 @@ async def create_entry_receive_transaction_date(
         "При необходимости добавьте комментарий к транзакции\n"
         "Отправьте точку `.`, если комментарий не требуется"
     )
-    await state.set_state(EntryCreateState.description)
+    await state.set_state(states.CreateEntry.description)
 
 
 @router.message(
-    EntryCreateState.description,
-    flags=ModelManagerStore.as_flags("entry", "category"),
+    states.CreateEntry.description,
 )
 async def create_entry_receive_description(
     message: types.Message,
     state: FSMContext,
-    entry_manager: EntryManager,
-    category_manager: DateQueryManager,
+    user: User,
+    ca_repository: CategoryRepository,
+    en_repository: EntryRepository,
 ):
     description = message.text
     if description == ".":
         description = None
 
     data = await state.get_data()
-    budget = data["budget"]
     category = data["category"]
     sum_ = data["sum"]
     date = data["transaction_date"]
-    created = entry_manager.create(
-        budget_id=budget.id,
+    created = en_repository.create_entry(
+        user.id,
         category_id=category.id,
         sum=sum_,
         transaction_date=date,
         description=description,
     )
     if created:
-        pretty_sum = f"{sum_ / 100:.2f}"
-        pretty_date = f"{date:%Y-%m-%d %H:%M:%S}"
-        success_msg = (
-            "Новая транзакция успешно создана.\n"
-            f"{pretty_sum} {budget.currency.value}, "
-            f"{category.name}, {pretty_date}"
+        await message.answer(
+            f"Новая транзакция успешно создана: {created.render()}"
         )
-        if description:
-            success_msg += f", {description}"
 
-        await message.answer(success_msg)
-
-        category_manager.update(
-            category.id, last_used=message.date.astimezone(settings.TIME_ZONE)
+        ca_repository.update_category(
+            category.id,
+            {
+                "last_used": message.date.astimezone(settings.TIME_ZONE),
+                "num_entries": category.num_entries + 1,
+            },
         )
     else:
         await message.answer(
@@ -188,43 +179,64 @@ async def entry_create(callback: types.CallbackQuery, state: FSMContext):
 
 
 @router.message(
-    Command("show_entries"), flags=ModelManagerStore.as_flags("budget")
+    Command("manage_entries"),
 )
-async def cmd_show_entries(
+async def cmd_manage_entries(
     message: types.Message,
     state: FSMContext,
     user: User,
-    budget_manager: DateQueryManager,
+    db_session: Session | scoped_session,
 ):
-    budgets = budget_manager.select(filters=[f"user_id=={user.id}"])
+    # budgets = get_user_budgets(db_session, user.id)
+    budgets = True
     await message.answer(
-        "Выберите бюджет для просмотра транзакций",
+        "Для просмотра транзакций выберите бюджет",
         reply_markup=keyboards.create_entry_show_budgets(budgets),
     )
-    await state.set_state(EntryList.budgets)
+    await state.set_state(states.PreProcessEntry.choose_budget)
 
 
 @router.callback_query(
-    EntryList.budgets,
+    states.PreProcessEntry.choose_budget,
     filters.EntryBudgetIdFilter(),
-    flags=ModelManagerStore.as_flags("entry"),
 )
-async def entry_list(
+async def manage_entry_choose_category(
     callback: types.CallbackQuery,
     state: FSMContext,
-    budget_id: int,
-    entry_manager: EntryManager,
+    user: User,
+    budget_id: str,
+    ca_repository: CategoryRepository,
 ):
-    entries = entry_manager.select(filters=[f"budget_id=={budget_id}"])
+    await state.update_data(budget_id=budget_id)
+    categories = ca_repository.get_user_categories(user.id)
+    await callback.message.answer(
+        "Выберите категорию",
+        reply_markup=keyboards.create_entry_show_categories(categories),
+    )
+    await state.set_state(states.PreProcessEntry.choose_category)
+
+
+@router.callback_query(
+    states.PreProcessEntry.choose_category, filters.EntryCategoryIdFilter()
+)
+async def manage_entry_choose_entry(
+    callback: types.CallbackQuery,
+    state: FSMContext,
+    category_id: int,
+    en_repository: EntryRepository,
+):
+    entries = en_repository.get_category_entries(category_id)
     await callback.message.answer(
         "Нажмите на транзакцию, чтобы выбрать действие",
         reply_markup=keyboards.entry_item_list_interactive(entries),
     )
-    await state.set_state(EntryList.entry_id)
     await callback.answer()
+    await state.set_state(states.PreProcessEntry.choose_entry)
 
 
-@router.callback_query(EntryList.entry_id, filters.GetEntryId())
+@router.callback_query(
+    states.PreProcessEntry.choose_entry, filters.GetEntryId()
+)
 async def entry_show_options(
     callback: types.CallbackQuery, state: FSMContext, entry_id: str
 ):
@@ -233,11 +245,12 @@ async def entry_show_options(
         "Выберите действие",
         reply_markup=keyboards.entry_item_choose_action2(),
     )
-    await state.set_state(EntryList.action)
+    await state.set_state(states.PreProcessEntry.choose_action)
 
 
 @router.callback_query(
-    EntryList.action, F.data.startswith("entry_item_action")
+    states.PreProcessEntry.choose_action,
+    F.data.startswith("entry_item_action"),
 )
 async def entry_delete_or_update(
     callback: types.CallbackQuery, state: FSMContext
@@ -248,25 +261,25 @@ async def entry_delete_or_update(
         entry_id = data["entry_id"]
         await callback.message.answer(
             "Вы уверены, что хотите удалить запись",
-            reply_markup=keyboards.confirm_delete(entry_id),
+            reply_markup=keyboards.entry_confirm_delete(entry_id),
         )
-        await state.set_state(EntryList.confirm_delete)
+        await state.clear()
+        await state.set_state(states.DeleteEntry.confirm)
     elif action == "update":
         pass
 
 
 @router.callback_query(
-    EntryList.confirm_delete,
+    states.DeleteEntry.confirm,
     filters.GetEntryId(),
-    flags=ModelManagerStore.as_flags("entry"),
 )
 async def entry_delete_confirm(
     callback: types.CallbackQuery,
     state: FSMContext,
-    entry_id: str,
-    entry_manager: EntryManager,
+    entry_id: int,
+    en_repository: EntryRepositoryMiddleWare,
 ):
-    deleted = entry_manager.delete(entry_id)
+    deleted = en_repository.delete_entry(entry_id)
     msg = (
         "Транзакция успешно удалена"
         if deleted
@@ -277,7 +290,7 @@ async def entry_delete_confirm(
     await state.clear()
 
 
-@router.callback_query(EntryList.confirm_delete)
+@router.callback_query(states.DeleteEntry.confirm)
 async def entry_delete_cancel(
     callback: types.CallbackQuery, state: FSMContext
 ):
@@ -286,14 +299,12 @@ async def entry_delete_cancel(
     await state.clear()
 
 
-@router.callback_query(
-    F.data == "entry_menu", flags=ModelManagerStore.as_flags("budget")
-)
+@router.callback_query(F.data == "entry_menu")
 async def show_entries(
     callback: types.CallbackQuery,
     state: FSMContext,
     user: User,
-    budget_manager: DateQueryManager,
+    db_session: Session | scoped_session,
 ):
-    await cmd_show_entries(callback.message, state, user, budget_manager)
+    await cmd_manage_entries(callback.message, state, user, db_session)
     await callback.answer()
