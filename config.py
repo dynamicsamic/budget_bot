1e41e30bcbf2c2b2cee3cb4571f413dece64354d
dynamicsamic <mirabyansr@gmail.com>
2024-01-28 01:20:55 +0300

    refactor: update config according to recent changes in pydantic_settings

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 from pydantic import SecretStr
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8"
+    )
+
     bot_token: SecretStr
     support_manager_id: SecretStr
-
-    class Config:
-        env_file = ".env"
-        env_file_encoding = "utf-8"
 
 
 config = Settings()
