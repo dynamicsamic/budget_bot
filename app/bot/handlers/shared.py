5da4fb7b6db30ed4a14313127ab56c9c969d075a
dynamicsamic <mirabyansr@gmail.com>
2024-02-03 19:13:09 +0300

    feat: move handler commands and callback prefixes to separate module

---
+++ app/bot/handlers/shared.py
@@ -0,0 +1,11 @@
+signup_user = "signup_user"
+
+create_category_command = "create_category"
+show_categories_command = "show_categories"
+create_category_callback = create_category_command
+show_categories_callback = show_categories_command
+category_id = "category_id"
+paginated_categories_page = "show_categories_page"
+select_category_type = "select_category_type"
+delete_category = "delete_category"
+update_category = "update_category"
