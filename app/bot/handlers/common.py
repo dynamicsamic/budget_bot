ab86fff2013d4a234804b23c7a09873644694057
dynamicsamic <mirabyansr@gmail.com>
2024-02-12 23:08:41 +0300

    refactor: update handlers with reusable text components

--- app/bot/handlers/common.py
+++ app/bot/handlers/common.py
@@ -5,6 +5,7 @@ from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 
 from app.bot.middlewares import UserRepositoryMiddleWare
+from app.bot.replies import prompts
 from app.bot.replies.keyboards.common import (
     show_main_menu,
     switch_to_main_or_cancel,
@@ -18,6 +19,8 @@ from app.bot.states import CreateUser
 from app.db.models import User
 from app.utils import aiogram_log_handler
 
+from . import shared
+
 logger = logging.getLogger(__name__)
 logger.addHandler(aiogram_log_handler)
 
@@ -30,60 +33,55 @@ async def cmd_test(message: types.Message, state: FSMContext):
     pass
 
 
-@router.message(Command("start"), flags={"allow_anonymous": True})
+@router.message(Command(shared.start_command), flags={"allow_anonymous": True})
 async def cmd_start(
     message: types.Message,
     state: FSMContext,
     user: User,
 ):
     if user.is_anonymous:
-        await state.set_state(CreateUser.choose_action)
+        await state.set_state(CreateUser.start)
         await message.answer(
-            """Вас приветсвует Бюджетный Менеджер!
-            Чтобы начать пользоваться менеджером, 
-            создайте новый бюджет в нужной вам валюте и несколько категорий
-            доходов и расходов для удобства.""",
+            prompts.start_message_anonymous,
             reply_markup=user_signup_menu,
         )
     elif not user.is_active:
         await message.answer(
-            """Вас приветсвует Бюджетный Менеджер!
-            Чтобы возобновить работу с менеджером, 
-            нажмите на кнопку `Активировать`.
-            """,
+            prompts.start_message_inactive,
             reply_markup=user_activation_menu,
         )
     else:
         await message.answer(
-            """С возвращением в Бюджетный Менеджер!
-            Продолжите работу в главном меню.
-            """,
+            prompts.start_message_active,
             reply_markup=switch_to_main_or_cancel,
         )
 
 
-@router.message(Command("cancel"), F.text.casefold() == "отмена")
+@router.message(Command(shared.cancel_command))
 async def cmd_cancel(message: types.Message, state: FSMContext):
     await state.clear()
     await message.answer(
-        text="Действие отменено", reply_markup=types.ReplyKeyboardRemove()
+        text=prompts.cancel_operation_note,
+        reply_markup=types.ReplyKeyboardRemove(),
     )
     logger.info("SUCCESS, operation canceled, state cleared")
 
 
-@router.callback_query(F.data.casefold() == "cancel")
+@router.callback_query(F.data.casefold() == shared.cancel_callback)
 async def cancel(callback: types.CallbackQuery, state: FSMContext):
     await state.clear()
     await callback.message.answer(
-        text="Действие отменено", reply_markup=types.ReplyKeyboardRemove()
+        text=prompts.cancel_operation_note,
+        reply_markup=types.ReplyKeyboardRemove(),
     )
     await callback.answer()
     logger.info("SUCCESS, operation canceled, state cleared")
 
 
-@router.message(Command("show_main_menu"))
+@router.message(Command(shared.show_main_menu_command))
 async def cmd_show_menu(message: types.Message):
-    await message.answer("Основное меню", reply_markup=show_main_menu)
+    await message.answer(prompts.main_menu_note, reply_markup=show_main_menu)
+    logger.info("SUCCESS")
 
 
 @router.message(Command("get_report"))
@@ -93,7 +91,7 @@ async def cmd_get_report(message: types.Message):
     )
 
 
-@router.callback_query(F.data == "main_menu_return")
+@router.callback_query(F.data == shared.show_main_menu_callback)
 async def return_to_main_menu(
     callback: types.CallbackQuery, state: FSMContext
 ):
