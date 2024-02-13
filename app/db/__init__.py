e3f33a4ec2f980e9d677dc161f16be2bc98211e9
dynamicsamic <mirabyansr@gmail.com>
2024-02-13 23:10:08 +0300

    refactor: update `db_session` context manager to reuse existing session

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -1,9 +1,14 @@
+import logging
 from contextlib import contextmanager
 
 from sqlalchemy import Engine, create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.orm import Session, scoped_session, sessionmaker
 
 from app import settings
+from app.utils import aiogram_log_handler
+
+logger = logging.getLogger(__name__)
+logger.addHandler(aiogram_log_handler)
 
 prod_engine = create_engine(
     settings.DATABASE["prod_db_url"], echo=settings.DEBUG
@@ -17,11 +22,27 @@ inmemory_test_engine = create_engine(
 
 
 @contextmanager
-def db_session(engine: Engine = test_engine):
-    Session = scoped_session(sessionmaker(bind=engine))
+def db_session(
+    engine: Engine = test_engine,
+    *,
+    existing_session: scoped_session | Session = None,
+):
+    if (
+        isinstance(existing_session, (scoped_session, Session))
+        and existing_session.is_active
+    ):
+        session = existing_session
+        logger.info("reuse existing db_session")
+    else:
+        session = scoped_session(sessionmaker(bind=engine))
+        logger.info("create new db_session")
+
     try:
-        yield Session
-    except Exception:
-        Session.rollback()
+        yield session
+    except Exception as e:
+        logger.exception(f"db session interupted with exception: {e}")
+        session.rollback()
+        raise e
     finally:
-        Session.close()
+        session.close()
+        logger.info("db_session closed")
