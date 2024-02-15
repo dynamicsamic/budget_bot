d3088dbb3f749e2ae3be4ac36d7a091256d0202e
dynamicsamic <mirabyansr@gmail.com>
2024-02-15 23:07:34 +0300

    refactor: update template imports

--- app/bot/handlers/common.py
+++ app/bot/handlers/common.py
@@ -5,9 +5,8 @@ from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 
 from app.bot.middlewares import UserRepositoryMiddleWare
-from app.bot.replies import prompts
 from app.bot.replies.keyboards.entry import cmd_report_kb
-from app.bot.replies.templates import (
+from app.bot.replies.templates.common import (
     cancel_operation,
     main_menu,
     start_message_active,
@@ -25,11 +24,6 @@ logger.addHandler(aiogram_log_handler)
 
 router = Router()
 router.message.middleware(UserRepositoryMiddleWare)
-
-
-@router.message(Command("test"))
-async def cmd_test(message: types.Message, state: FSMContext):
-    pass
 
 
 @router.message(Command(shared.start_command), flags={"allow_anonymous": True})
@@ -50,10 +44,7 @@ async def cmd_start(
 @router.message(Command(shared.cancel_command))
 async def cmd_cancel(message: types.Message, state: FSMContext):
     await state.clear()
-    await message.answer(
-        text=prompts.cancel_operation_note,
-        reply_markup=types.ReplyKeyboardRemove(),
-    )
+    await message.answer(**cancel_operation)
     logger.info("SUCCESS, operation canceled, state cleared")
 
 
@@ -85,3 +76,8 @@ async def return_to_main_menu(
     await state.clear()
     await cmd_show_menu(callback.message)
     await callback.answer()
+
+
+@router.message(Command("test"))
+async def cmd_test(message: types.Message, state: FSMContext):
+    pass
