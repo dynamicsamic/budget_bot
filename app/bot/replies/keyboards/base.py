93bc3b59177c77e77ce3ff78dceee1bfda7f443d
dynamicsamic <mirabyansr@gmail.com>
2024-03-04 21:55:44 +0300

    refactor: clean up from duplicate and commented code

--- app/bot/replies/keyboards/base.py
+++ app/bot/replies/keyboards/base.py
@@ -16,10 +16,12 @@ def button_menu(
 
 
 def interactive_item_list(
-    items: Iterable[_BaseModel],
     callback_prefix: str,
-    extra_buttons: list[InlineKeyboardButton] = None,
+    items: Iterable[_BaseModel],
+    *,
     adjust: int = 1,
+    paginator: OffsetPaginator | None = None,
+    extra_buttons: list[InlineKeyboardButton] | None = None,
 ) -> InlineKeyboardMarkup:
     buttons = [
         [
@@ -30,47 +32,25 @@ def interactive_item_list(
             for item in items
         ]
     ]
-    if extra_buttons:
-        buttons.append(extra_buttons)
 
-    builder = InlineKeyboardBuilder(buttons)
-    builder.adjust(adjust)
-    return builder.as_markup()
-
-
-def paginated_item_list(
-    items: Iterable[_BaseModel],
-    callback_prefix: str,
-    paginator: OffsetPaginator,
-    extra_buttons: list[InlineKeyboardButton] = None,
-    adjust: int = 1,
-) -> InlineKeyboardMarkup:
-    buttons = [
-        [
-            InlineKeyboardButton(
-                text=item.render(),
-                callback_data=f"{callback_prefix}:{item.id}",
-            )
-            for item in items
-        ]
-    ]
     extra_buttons = extra_buttons or []
 
-    if paginator.prev_page_offset is not None:
-        extra_buttons.append(
-            InlineKeyboardButton(
-                text="Предыдущие",
-                callback_data=f"{paginator.callback_prefix}:previous",
+    if paginator:
+        if paginator.prev_page_offset is not None:
+            extra_buttons.append(
+                InlineKeyboardButton(
+                    text="Предыдущие",
+                    callback_data=f"{paginator.callback_prefix}:previous",
+                )
             )
-        )
 
-    if paginator.next_page_offset is not None:
-        extra_buttons.append(
-            InlineKeyboardButton(
-                text="Следующие",
-                callback_data=f"{paginator.callback_prefix}:next",
+        if paginator.next_page_offset is not None:
+            extra_buttons.append(
+                InlineKeyboardButton(
+                    text="Следующие",
+                    callback_data=f"{paginator.callback_prefix}:next",
+                )
             )
-        )
 
     buttons.append(extra_buttons)
 
@@ -82,10 +62,8 @@ def paginated_item_list(
 def create_callback_buttons(
     button_names: dict[str, str],
     callback_prefix: str,
-    extra_buttons: list[InlineKeyboardButton] = None,
-):
-    # builder = InlineKeyboardBuilder()
-
+    extra_buttons: list[InlineKeyboardButton] | None = None,
+) -> InlineKeyboardMarkup:
     buttons = [
         [
             InlineKeyboardButton(
@@ -95,12 +73,6 @@ def create_callback_buttons(
             for button_name, callback_suffix in button_names.items()
         ]
     ]
-
-    # for button_name, callback_suffix in button_names.items():
-    #     builder.button(
-    #         text=button_name.capitalize(),
-    #         callback_data=f"{callback_prefix}:{callback_suffix.lower()}",
-    #     )
 
     if extra_buttons:
         buttons.append(extra_buttons)
