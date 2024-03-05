1ab08e8b610c69cb1325cd560c389142601e9108
dynamicsamic <mirabyansr@gmail.com>
2024-03-05 22:43:39 +0300

    refactor: rename `shared` module to `string_constants`; uppercase constants

---
+++ app/bot/string_constants.py
@@ -0,0 +1,22 @@
+START_COMMAND = "start"
+CANCEL_COMMAND = "cancel"
+SHOW_MAIN_MENU_COMMAND = "show_main_menu"
+CANCEL_CALL = CANCEL_COMMAND
+SHOW_MAIN_MENU_CALL = SHOW_MAIN_MENU_COMMAND
+
+SIGNUP_USER = "signup_user"
+ACTIVATE_USER = "activate_user"
+DELETE_USER = "delete_user"
+UPDATE_USER = "update_user"
+SHOW_USER_PROFILE = "show_user_profile"
+UPDATE_CURRENCY = "update_currency"
+
+CREATE_CATEGORY_COMMAND = "create_category"
+SHOW_CATEGORIES_COMMAND = "show_categories"
+CREATE_CATEGORY_CALL = CREATE_CATEGORY_COMMAND
+SHOW_CATEGORIES_CALL = SHOW_CATEGORIES_COMMAND
+CATEGORY_ID = "category_id"
+PAGINATED_CATEGORIES_PAGE = "show_categories_page"
+SELECT_CATEGORY_TYPE = "select_category_type"
+DELETE_CATEGORY = "delete_category"
+UPDATE_CATEGORY = "update_category"
