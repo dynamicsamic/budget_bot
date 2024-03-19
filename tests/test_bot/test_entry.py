a0e22fbba8a8086311414d68ac73867e3234f1fc
dynamicsamic <mirabyansr@gmail.com>
2024-03-19 22:56:29 +0300

    tests: add first tests for entry handlers

---
+++ tests/test_bot/test_entry.py
@@ -0,0 +1,54 @@
+from datetime import datetime
+
+import pytest
+from aiogram.methods import SendMessage
+from aiogram.types import Message, Update
+
+from app.bot import string_constants as sc
+from app.bot.templates import func
+from app.db.models import CategoryType
+from app.db.repository import CategoryRepository
+from app.utils import OffsetPaginator
+
+from ..test_utils import INCOME_SAMPLE, TARGET_USER_ID, assert_uses_template
+from .conftest import chat, user
+
+
+@pytest.fixture
+def category_repo(persistent_db_session):
+    return CategoryRepository(persistent_db_session)
+
+
+@pytest.mark.asyncio
+async def test_create_income(create_test_data, category_repo, requester):
+    msg = Message(
+        message_id=1,
+        date=datetime.now(),
+        from_user=user,
+        chat=chat,
+        text=f"/{sc.CREATE_INCOME_COMMAND}",
+    )
+
+    await requester.make_request(
+        SendMessage,
+        Update(
+            update_id=1,
+            message=msg,
+        ),
+    )
+
+    page_limit = 5
+    paginator = OffsetPaginator(
+        sc.ENTRY_INCOME_CATEGORY_PAGE, INCOME_SAMPLE, page_limit
+    )
+    categories = category_repo.get_user_categories(
+        TARGET_USER_ID, category_type=CategoryType.INCOME
+    )
+
+    answer = requester.read_last_sent_message()
+    assert_uses_template(
+        answer,
+        func.show_paginated_income,
+        categories=categories.result,
+        paginator=paginator,
+    )
